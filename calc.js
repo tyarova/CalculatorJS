979049c700c209ca6ac95be257b3a77c2b8543ee
tetyana.yarova <tetyana.yarova@zoomdata.com>
2018-12-30 01:17:22 +0200

    Added calculate function

--- calc.js
+++ calc.js
@@ -1,30 +1,90 @@
 
-var clearButton = document.getElementById("clr");
-var displayField = document.getElementById("display");
-var calculateButton = document.getElementById("equal");
+var keys = document.querySelector('.calculationKeyboard');
 
-var calculationFinished = false;
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
 
-$('[name = number], [name = operator]')
-    .click((e)=> insert(e.target.value));
+  keys.addEventListener('click', (event)=>{
+      let val = event.target.value;
+    if (event.target.classList.contains('number')) {
+        inputNumber(val);
+        console.log(calculator);
+        updateDisplay();
+        return;
+      }
+      if (event.target.classList.contains('operator')) {
+        calculate(val);
+        console.log(calculator);
+        updateDisplay();
+        return;
+      }
+      if (event.target.classList.contains('dot')) {
+        inputDot(val);
+        updateDisplay();
+        return;
+      }
+      if (event.target.classList.contains('clr')) {
+        clearDisplay();
+        updateDisplay();
+        return;
+      }
 
-function insert(num){
-    var formElement = document.form[0];
-    if (calculationFinished == true) {
-        formElement.value = num;
-        calculationFinished = false;
-    }else{
-    formElement.value = formElement.value + num; 
-}
+  });
+
+  function updateDisplay() {
+    let display = document.form[0];
+    display.value = calculator.displayValue;
+  };
+
+  let performCalculation = {
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+  
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+  
+    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+  
+    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+  
+    '=': (firstOperand, secondOperand) => secondOperand
+  };
+
+  function calculate(nextOperator) {
+    let { firstOperand, displayValue, operator } = calculator;
+    let inputValue = parseFloat(displayValue);
+    if (firstOperand === null) {
+      calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const result = performCalculation[operator](firstOperand, inputValue);
+    
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+      }
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+  }
+
+  function inputNumber(num) {
+      let { displayValue, waitingForSecondOperand } = calculator;
+      if (waitingForSecondOperand === true) {
+        calculator.displayValue = num;
+        calculator.waitingForSecondOperand = false;
+      } else {
+    calculator.displayValue = displayValue === '0' ? num : displayValue + num;
+  } 
 };
 
-calculateButton.addEventListener('click', function calculate(){
-    displayField.value = eval(displayField.value);
-    calculationFinished = true;
-});
+  function inputDot(dot) {
+    if (!calculator.displayValue.includes(dot)) {
+      calculator.displayValue += dot;
+    }
+  }
 
-function clearDisplay(){
-    displayField.value = "";
-};
+  function clearDisplay(){
+    calculator.displayValue = '0';
+  }
 
-clearButton.addEventListener('click', clearDisplay);
+
